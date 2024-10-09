f6b99bc32dc3a678a2d4159edae429b0ffcbaa02
scotthops <scottyhops@gmail.com>
2024-10-09 13:39:42 -0700

    assign2commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+// ASSIGN1
 // GET /foo -> "bar"
 app.get('/foo', (req, res) => {
     res.send('bar');
@@ -17,6 +18,25 @@ app.post('/hello', (req, res) => {
 app.get('/kill', (req, res) => {
     res.send('Shutting down...');
     process.exit();
+});
+
+//ASSIGN2
+// GET /configValue -> Return 'configValue'
+app.get('/configValue', (req, res) => {
+    const configValue = process.env.CONFIG_VALUE || 'No Config Value Set';
+    res.send(configValue);
+});
+
+// GET /secretValue -> Return the value of 'secretValue'
+app.get('/secretValue', (req, res) => {
+    const secretValue = process.env.SECRET_VALUE || 'No Secret Value Set';
+    res.send(secretValue);
+});
+
+// GET /envValue -> Return the value of 'envValue'
+app.get('/envValue', (req, res) => {
+    const envValue = process.env.ENV_VALUE || 'No Env Value Set';
+    res.send(envValue);
 });
 
 // Start the server
